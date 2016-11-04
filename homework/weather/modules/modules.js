771bb2fa7971bfa58c830435fe92343bf8f790e4
Byggfut <jossten.childs@gmail.com>
2016-11-03 19:11:21 -0600

    Update

---
+++ homework/weather/modules/modules.js
@@ -0,0 +1,5 @@
+$(function() {
+    $("#page-nav").load("/homework/weather/modules/nav.html");
+    $("#page-header").load("/homework/weather/modules/header.html");
+    $("#page-footer").load("/homework/weather/modules/footer.html");
+});
