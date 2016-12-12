bf298c0a95f53c69546ad9d1b69a57c2c3dc8564
Byggfut <jossten.childs@gmail.com>
2016-12-11 19:47:28 -0700

    slight change

--- final/modules/remotewu.js
+++ final/modules/remotewu.js
@@ -4,24 +4,23 @@
 //    $(".Date1").text(data.lesson2.Date);
 //});
 //});
-//
-$.getJSON( "../final/modules/guitar.json", function( data ) {
-//    console.log(data)
-//    var items = [];
-//    $.each( data, function( key, val ) {
-//        items.push( '<source + src= ' + key + type + "type=video/mp4" + key + "'src'" + val + "/>" );
-//    });
-//
-//    $( "<div/>", {
-//        "class": "my-new-list",
-//        html: items.join( "" )
-//    }).appendTo( "source" );
-//});
-document.getElementById("demo").innerHTML = fruits;
-    $.getJSON( "../final/modules/guitar.json", function( data ) {
-var fruits = [""];
-function myFunction() {
-    fruits.push("Kiwi");
-    document.getElementById("demo").innerHTML = fruits;
-    };
+
+$.getJSON( "../final/modules/guitar1.json", function( data ) {
+    var items = [];
+    $.each( data, function( key, val ) {
+        items.push( "<video" + key + "'src='" + val + ">" + "'</video>" );
     });
+
+    $( "<div/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+    }).appendTo( "div.banner-section" );
+});
+//document.getElementById("demo").innerHTML = fruits;
+//    $.getJSON( "../final/modules/guitar.json", function( data ) {
+//var fruits = [""];
+//function myFunction() {
+//    fruits.push("Kiwi");
+//    document.getElementById("demo").innerHTML = fruits;
+//    };
+//    });
