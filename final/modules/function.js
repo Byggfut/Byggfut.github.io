ec47a91f5e93df9ba23501eb6a78d0f5f72d4453
Byggfut <jossten.childs@gmail.com>
2016-12-13 17:56:20 -0700

    style cahnges

--- final/modules/function.js
+++ final/modules/function.js
@@ -1,16 +1,15 @@
-$(function(){
-    console.log("run");
-    $.getJSON( "/final/modules/blogjson.json", function( data ) {
-        console.log(data);
-        var items = [];
-        console.log("anything");
-        $.each( data, function( key, val ) {
-            items.push( "<li id='" + key + "'>" + val + "</li>" );
+$(document).ready(function(){
+    $.getJSON( "/final/modules/blog.json", function( data ) {
+            console.log(data);
+            $('.date').text(data.blog1.date);
+            $('.topic').text(data.blog1.topic);
+            $('.details').text(data.blog1.details);
+            $('.summary').text(data.blog1.summary);
+            $('.article').text(data.blog1.article);
+            $('.date1').text(data.blog2.date);
+            $('.topic1').text(data.blog2.topic);
+            $('.details1').text(data.blog2.details);
+            $('.summary1').text(data.blog2.summary)
+            $('.article1').text(data.blog2.article);
         });
-
-        $( "<ul/>", {
-            "class": "mynewlist",
-            html: items.join( "" )
-        }).appendTo( "div.links" );
     });
-});
--- final/modules/remotewu.js
+++ final/modules/remotewu.js
@@ -1,13 +1,15 @@
-$.getJSON( "../final/modules/guitar1.json", function( data ) {
+$.getJSON( "/final/modules/guitar1.json", function( data ) {
+    console.log(data);
     var items = [];
     $.each( data, function( key, val ) {
-        items.push( "<video" + key + "'src='" + val + ">" + "'</video>" );
+        items.push( "<video class='lessonvideo' src='" + val + "' controls> </video>" );
     });
+
 
     $( "<div/>", {
         "class": "my-new-list",
         html: items.join( "" )
-    }).appendTo( "div.banner-section" );
+    }).appendTo( "div.videoselector" );
 });
 //document.getElementById("demo").innerHTML = fruits;
 //    $.getJSON( "../final/modules/guitar.json", function( data ) {
